6f9cf65d9f0e40ef763f5eb31dfaa5be7d56c2b4
code-with-Anson <2795757650@qq.com>
2025-01-06 23:13:10 +0800

    初始化项目工程

---
+++ src/main/java/com/shiro/backend/ShiroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shiro.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroBackendApplication.class, args);
+    }
+
+}
